ad13f26ba1aa6b4eeae753414d6d7a46c2bc0a16
Sam Coe <samcoe@users.noreply.github.com>
2020-10-14 15:45:35 +0200

    Generate help topic markdown pages

--- cmd/gen-docs/main.go
+++ cmd/gen-docs/main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/cli/cli/pkg/cmd/root"
 	"github.com/cli/cli/pkg/cmdutil"
 	"github.com/cli/cli/pkg/iostreams"
+	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 	"github.com/spf13/pflag"
 )
 
 func main() {
-
 	var flagError pflag.ErrorHandling
 	docCmd := pflag.NewFlagSet("", flagError)
 	manPage := docCmd.BoolP("man-page", "", false, "Generate manual pages")
@@ -50,6 +51,11 @@ func main() {
 		if err != nil {
 			fatal(err)
 		}
+
+		err = genHelpMarkdown(rootCmd, *dir)
+		if err != nil {
+			fatal(err)
+		}
 	}
 
 	if *manPage {
@@ -82,4 +88,37 @@ func linkHandler(name string) string {
 func fatal(msg interface{}) {
 	fmt.Fprintln(os.Stderr, msg)
 	os.Exit(1)
+}
+
+func genHelpMarkdown(cmd *cobra.Command, dir string) error {
+	var baseHelpCmd *cobra.Command
+	commands := cmd.Commands()
+	for _, command := range commands {
+		if command.Name() == "help" {
+			baseHelpCmd = command
+			break
+		}
+	}
+	environmentCmd := root.NewHelpTopic("environment")
+	helpCommands := []*cobra.Command{baseHelpCmd, environmentCmd}
+	for _, command := range helpCommands {
+		filename := "gh_help.md"
+		if command.Name() != "help" {
+			filename = "gh_help_" + command.Name() + ".md"
+		}
+		path := filepath.Join(dir, filename)
+		f, err := os.Create(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		if _, err := f.WriteString(filePrepender(filename)); err != nil {
+			return err
+		}
+		err = doc.GenMarkdownCustom(command, f, linkHandler)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
